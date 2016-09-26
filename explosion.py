7e15ae4056fbf628b764ee3b1fd6e93533c05324
MirjamFi <mirjam.figaschewski@student.uni-tuebingen.de>
2016-09-26 10:38:51 +0200

    Added "blocking constraint"
    When complex is exploded, chains and ligands must be at least 3 Angstrom
    apart. This  is enforced iteratively.

--- explosion.py
+++ explosion.py
@@ -1,16 +1,17 @@
 # run C:/Users/Figaschewski/Dropbox/Masterarbeit/Masterthesis/explosion.py
 
 from pymol import cmd 
-import get_colors
 import math
-import center_of_mass as cenma
+import center_of_mass as cenma ## calulate center of mass
+from pymol import stored ## import stored for passing data back and forth
+from pymol import util ## color by chain
+import get_colors ## get random colors
+import time
+
+start_time = time.clock()
 
 cmd.delete('all')
 cmd.set('ignore_case', 'off') ## case sensitive for chain ids
-
-''' show axes '''
-# import draw_axes as da
-# da.axes()
 
 ''' load complex from PDB'''
 pdbName = '3oaa'
@@ -19,16 +20,12 @@ cmd.fetch(pdbName, type='pdb1')
 cmd.set('all_states', 'on')
 cmd.remove('solvent')
 cmd.show('spheres', 'organic')
+util.cbc(selection= pdbName)
 
 ''' get chains of complex'''
 chains = cmd.get_chains(pdbName)
 
-#'''create object for each chain in selection '''
-# cmd.split_chains(pdbName)
-
 ''' get all ligands (organic) from complex'''
-"""import stored for passing data back and forth"""
-from pymol import stored
 stored.ligands = []
 
 ## create the original selection of all organic atoms 
@@ -54,28 +51,39 @@ cmd.delete('allOrg')
 cmd.delete('temp')
 
 ''' translate an object relative to complex of origin using center of mass'''
-def translate_selection(original_xyz, trans_xyz, transname, factor):
+def translate_selection(originXYZ, transXYZ, transname, factor):
+	
 	## vector between COMs to translate chain
-	dist=math.sqrt((trans_xyz[0]-original_xyz[0])**2 +
-					(trans_xyz[1]-original_xyz[1])**2 +
-					(trans_xyz[2]-original_xyz[2])**2)
+	dist=math.sqrt((transXYZ[0]-originXYZ[0])**2 +
+					(transXYZ[1]-originXYZ[1])**2 +
+					(transXYZ[2]-originXYZ[2])**2)
 
-	vector=((trans_xyz[0]-original_xyz[0])/dist, 
-			(trans_xyz[1]-original_xyz[1])/dist, 
-			(trans_xyz[2]-original_xyz[2])/dist)
+	vector=((transXYZ[0]-originXYZ[0])/dist, 
+			(transXYZ[1]-originXYZ[1])/dist, 
+			(transXYZ[2]-originXYZ[2])/dist)
 	trans_vec = [x * factor for x in vector] 
 
 	## translate chain with vector
 	cmd.translate(trans_vec,transname, camera=0)
-
+	
+'''calculate COM of an object '''
+def calc_COM(transname):
+		cenma.com(transname, state = 1)
+		cmd.zoom(transname + '_COM')
+		pos = cmd.get_position(transname + '_COM')
+		cmd.delete(transname + '_COM')
+		return pos
+		
 ''' Explosion'''
+
 ## create models for all states of complex
 states = cmd.split_states(pdbName)
 
-"""for each state create an object of every chain. In this state
- calulate COM for state and according chains and translate chain along 
- vector between COMs of state and chain"""
- 
+"""for each state create an object of every chain and for related ligands. 
+	In this state calulate COM for state and according chains and translate 
+	chain along vector between COMs of state and chain. 
+	For every chain do the same with its ligands.
+"""
 com_num = ""
 for state in range(1,cmd.count_states()+1):
 	## state = 0 would calculate the COM for all states at once
@@ -88,65 +96,87 @@ for state in range(1,cmd.count_states()+1):
 		s = "0" + s
 		
 	## COM of state (all chains)
-	cenma.com(pdbName + "_" + s, state = 1)
-	
-	## get coordinates of COM
-	cmd.zoom(pdbName + "_" + s + '_COM')
-	complex_xyz = cmd.get_position(pdbName + '_COM' + com_num)
-	
+	complexXYZ = calc_COM(pdbName + "_" + s)
+
 	## calculate COM for single chains in state
+	cNames = []
+	chainsCOMS = {}
 	for c in chains:
-	
 		## color each chain individually
-		cmd.color(get_colors.get_random_color(), pdbName + "_" + s + "& chain " + c)
+		if cmd.count_states() > 1:
+			cmd.color(get_colors.get_random_color(), pdbName + "_" + s + "& chain " + c)
 
 		## create object for chain with name
 		chainname = s + '_'+c
-
+		cNames.append(chainname)
 		origin = pdbName+'_'+s+ ' & chain '+c
 		cmd.create(chainname, origin)
 		
+		## calculate and save COM of chain
+		chainsCOMS[chainname] = calc_COM(chainname)		
+	
+	## translate chains iteratively to asure a distance of 3 Angstrom between chains
+	for chainname in cNames:
 		## COM of chain
-		cenma.com(chainname, state = 1)
-		cmd.zoom(chainname + '_COM')
-		chain_xyz = cmd.get_position(chainname + '_COM')
+		chainXYZ = chainsCOMS[chainname]
 		
+		## list of all chains to calculate distances after translation
+		chainsDist = dict.fromkeys(cNames, 100)  
+		chainsDist[chainname] = 0
+
 		## only translate chain if there are multiple chains
 		if len(chains) > 1:	
-			## translate chain
-			translate_selection(complex_xyz, chain_xyz, chainname, 50)
-			cmd.delete(chainname + '_COM')
 		
+			## translate chain, so that it has at least 3Angstrom distance to
+			## all other chains
+			while any(x>0 for x in chainsDist.values()):
+				for key in chainsDist.keys():
+					if key != chainname:
+					
+						## number of atoms in radius of 3 Angstrom
+						nearC = key + ' w. 3 of ' + chainname
+						chainsDist[key] = cmd.count_atoms(nearC)
+						
+						## if number of atoms > 0, translate all chains 
+						## (to keep shape of complex)
+						if cmd.count_atoms(nearC) > 0:
+		
+							## translate other chains
+							for cname in cNames:
+								cXYZ = chainsCOMS[cname]
+								translate_selection(complexXYZ, cXYZ, cname, 20)
+								cmd.delete(cname + '_COM')
+
+	## translate ligands (also distance of 3 Angstrom to chains)
+	for chainname in cNames:
+			
 		## translate ligands on current chain, if ligands are present
 		if stored.ligands:
-		
-			## if chain has been translated, calculate COM of translated chain
-			if len(chains) > 1:
-				cenma.com(chainname, state = 1)
-				cmd.zoom(chainname + '_COM')
-				chain_xyz = cmd.get_position(chainname + '_COM')
-					
-			## delete chain's COM
-			cmd.delete(chainname + '_COM')
 			
-			## select ligand on chain
+			## select ligand on chain	
 			selection = chainname + " & organic"
 			ligandname = chainname + "_org"
-			
+
 			## only translate if chain really has a ligand
 			if cmd.count_atoms(selection) > 0:
 				cmd.create(ligandname, selection)
-				
+		
 				## calculate COM of ligand
-				cenma.com(ligandname, state = 1)
-				cmd.zoom(ligandname + '_COM')
-				ligand_xyz = cmd.get_position(ligandname + '_COM')
+				ligandXYZ = calc_COM(ligandname)
 				
-				## translate ligand
-				translate_selection(chain_xyz, ligand_xyz, ligandname, 30)
-
-				## delete ligand's COM
-				cmd.delete(ligandname + '_COM')
+				## if chain got translated, claculate new COM for translation of ligands
+				if len(chains) > 1:
+					chainsCOMS[chainname] = calc_COM(chainname)
+					chainXYZ = chainsCOMS[chainname]
+			
+				## number of atoms in radius of 3 Angstrom
+				nearC = ligandname + ' w. 3 of ' + chainname
+				
+				## asure distance of 3 Angstrom between ligand and chain
+				while cmd.count_atoms(nearC) > 0:
+					## translate ligand
+					translate_selection(chainXYZ, ligandXYZ, ligandname, 10)
+					nearC = ligandname + ' w. 3 of ' + chainname
 			
 				## color binding site
 				binding = 'byres (' + chainname + ' nto. 3 of organic)'
@@ -156,15 +186,11 @@ for state in range(1,cmd.count_states()+1):
 				
 				## remove ligand from translated chain
 				cmd.remove(selection)	
-				
-		## delete chain's COM
-		cmd.delete(chainname + '_COM')
-	
+					
 	## delete state object
-	cmd.delete(pdbName + "_"+ s)
-	
-	## delete state's COM
-	cmd.delete(pdbName + "_" + s + '_COM')
-		
+	cmd.delete(pdbName + "_"+ s)	
+
 cmd.zoom('all')
+
+print time.clock() - start_time, "seconds"
 			
